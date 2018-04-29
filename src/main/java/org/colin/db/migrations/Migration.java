76b89a1ee348badb0909c56bd31434b60156c4a2
Colin James <obscurecolin@gmail.com>
2018-04-29 18:01:44 +0100

    Documented return values for migration creations/reversals.

--- src/main/java/org/colin/db/migrations/Migration.java
+++ src/main/java/org/colin/db/migrations/Migration.java
@@ -10,7 +10,8 @@ public abstract class Migration {
 
     /**
      * Common constructor for dependency injection of database connection (see {@link DBConnection}) for easier testing.
-     * @param connection
+     *
+     * @param connection dependency injectable connection to be used by the migration
      */
     public Migration(DBConnection connection) {
         this.connection = connection;
@@ -18,11 +19,15 @@ public abstract class Migration {
 
     /**
      * The method is for creational actions upon the database schema.
+     *
+     * @return whether the migration was completed successfully
      */
     public abstract boolean up();
 
     /**
      * The method is for reversing the actions of {@link Migration#up()}.
+     *
+     * @return whether the migration's reversal was completed successfully
      */
     public abstract boolean down();
 }
