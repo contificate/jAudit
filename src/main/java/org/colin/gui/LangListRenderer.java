bb1859c6ea11ad0d3e699a80339fe2dffbac373f
Colin James <obscurecolin@gmail.com>
2018-04-29 18:02:14 +0100

    Documentation for language selection renderer and model.

--- src/main/java/org/colin/gui/LangListRenderer.java
+++ src/main/java/org/colin/gui/LangListRenderer.java
@@ -1,22 +1,36 @@
 package org.colin.gui;
 
-import com.alee.utils.ColorUtils;
-import org.colin.util.ColourUtil;
-
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
+/**
+ * Renderer used by {@link org.colin.gui.views.LangSelectionView}
+ */
 public class LangListRenderer extends JLabel implements ListCellRenderer<LanguageOption> {
-
+    /**
+     * Get the component used to render the list item.
+     * In this case, the component is the super class, {@link JLabel}.
+     *
+     * @param list           list of options
+     * @param languageOption the language option being rendered
+     * @param index          the index of the language option being rendered
+     * @param selected       true if the item is selected
+     * @param focused        true if the item is focused
+     * @return component to be rendered on behalf of the {@link LanguageOption} list item
+     */
     @Override
-    public Component getListCellRendererComponent(JList<? extends LanguageOption> list, LanguageOption languageOption, int i, boolean selected, boolean focused) {
+    public Component getListCellRendererComponent(JList<? extends LanguageOption> list, LanguageOption languageOption, int index, boolean selected, boolean focused) {
+        // set the label's text to the language string
         setText(languageOption.getLanguage());
+
+        // set the label's icon to the flag
         setIcon(languageOption.getFlag());
+
+        // add an empty border for aesthetics
         setBorder(new EmptyBorder(5, 5, 5, 5));
-        if(selected) {
-            setBackground(ColourUtil.fromHex("#00ff00"));
-        }
+
+        // return this, e.g. the label being rendered
         return this;
     }
 }
--- src/main/java/org/colin/gui/LanguageOption.java
+++ src/main/java/org/colin/gui/LanguageOption.java
@@ -1,27 +1,63 @@
 package org.colin.gui;
+
 import javax.swing.*;
 import java.util.Locale;
 
+/**
+ * Class represents a language option, used by {@link org.colin.gui.views.LangSelectionView}
+ */
 public class LanguageOption {
-
+    /**
+     * The language being represented
+     */
     private String language;
+
+    /**
+     * The flag icon for the language's region
+     */
     private ImageIcon flag;
+
+    /**
+     * The locale for the language, used for localisation
+     */
     private Locale locale;
 
+    /**
+     * Initialise the <b>immutable</b> language option.
+     *
+     * @param language language string e.g. English, Deutsch, etc.
+     * @param flag     flag icon for the language's region
+     * @param locale   the locale being represented by the option
+     */
     public LanguageOption(final String language, final ImageIcon flag, final Locale locale) {
         this.language = language;
         this.flag = flag;
         this.locale = locale;
     }
 
+    /**
+     * Get the language string
+     *
+     * @return language string
+     */
     public String getLanguage() {
         return language;
     }
 
+    /**
+     * Get the flag
+     *
+     * @return the language icon
+     */
     public ImageIcon getFlag() {
         return flag;
     }
 
+    /**
+     * Get the locale
+     *
+     * @return the locale
+     */
     public Locale getLocale() {
         return locale;
     }
